e5f09ca685ac1ce8d8c95cee82500829408c67df
Sardor <139249785+sardornext@users.noreply.github.com>
2024-08-27 11:51:08 +0500

    add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,28 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min = 0;
+        this.max = 0;
+        this.currentGuess = 0;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
+        this.currentGuess = Math.round((this.min + this.max) / 2);
     }
 
     guess() {
-
+        return this.currentGuess;
     }
 
     lower() {
-
+        this.max = this.currentGuess;
+        this.currentGuess = Math.round((this.min + this.max) / 2);
     }
 
     greater() {
-
+        this.min = this.currentGuess;
+        this.currentGuess = Math.round((this.min + this.max) / 2);
     }
 }
 
